965e37abfb3a62f7d995d2c9dd51033796d7715d
lay <layliang@qq.com>
2019-02-15 21:21:08 +0800

    Initial commit

---
+++ src/test/java/com/lovely/question/QuestionApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lovely.question;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuestionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
